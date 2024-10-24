0c8396bc2ffa89c678e87ce8f41b97afde2a78e7
keziaeci <keziaeci2806@gmail.com>
2024-10-25 06:24:02 +0700

    add clock and fix view

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import React,{ useEffect,useState }  from 'react'
+import { TextField, Spinner } from '@radix-ui/themes'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 
 export const Home = () => {
     const [quote, setQuote] = useState([])
@@ -42,24 +44,63 @@ export const Home = () => {
     },[])
     // console.log(image.urls.raw)
 
+    // const getSearch = (query) => {
+
+    //     console.log(query)
+    // }
+    // const date = Date.now();
+    // const
+
+    const getClock = setInterval(() => {
+        let now = new Date()
+        let hours = now.getHours().toString().padStart(2,'0')
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        let seconds = now.getSeconds().toString().padStart(2, '0');
+            
+        let timeString = `${hours}:${minutes}:${seconds}`;
+        // console.log(timeString)
+        document.getElementById('clock').textContent = timeString
+    }, 1000);
+
+    // getClock()
+    // window.load
+
     return (
         <>
-        <div className='flex bg-red-400 min-w-full min-h-screen m-0 p-0'>
-            <div>
-                <h1>Hi there!</h1>
-                {load ? (
-                    <p>loding</p>
-                ) : (
-                    // <div className='bg-[url("https://random-image-pepebigotes.vercel.app/api/random-image")]'>
-                    <div>
-                        {/* <img className='object-cover' src="https://random-image-pepebigotes.vercel.app/api/random-image" alt="" /> */}
-                        {/* <img className='object-cover' src={image.url} alt="" /> */}
-                        {/* <img className='object-cover' src={image.urls?.regular} alt="" /> */}
-                        <p>"{quote.quote}"</p>
-                        <p>- {quote.author}</p>
-                    </div>
+        <div className='flex min-w-full flex-col min-h-screen  m-0 p-0 justify-center items-center'>
+            <div className='flex flex-col justify-between min-w-full gap-5 '>
+
+                <div className='flex flex-col gap-5'>
+                    <h1 className='font-semibold text-3xl text-gray-400'>Hi there!</h1>
+                    <p id='clock' className='font-bold text-7xl '></p>
+
+                    <form className='mt-5' onSubmit={(e) => {
+                        e.preventDefault()
+                        const searchValue = e.target.elements.search.value
+                        console.log(searchValue)
+                        window.location.href = `https://www.google.com/search?q=${searchValue}`
+                    }}>
+                        <TextField.Root type="text"  name="search" placeholder="blablabla...">
+                            <TextField.Slot >
+                                <MagnifyingGlassIcon height="16" width="16" />
+                            </TextField.Slot>
+                        </TextField.Root>
+                    </form>
+                </div>
+
+            </div>
+
+            <div className='mt-10'>
+            {load ? (
+                <Spinner />
+            ) : (
+                    <>
+                        <p className='font-thin text-lg'>"{quote.quote}"</p>
+                        <p className='font-thin text-sm text-gray-400'>- {quote.author}</p>
+                    </>
                 )}
             </div>
+
             
         </div>
         </>
